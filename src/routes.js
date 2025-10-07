bdcae5f1fe7b9bf999a0732978b91d5337ebb7ca
vitorcosta039 <vitorcosta588@gmail.com>
2025-10-07 20:08:20 -0300

    GET search /tasks

--- src/routes.js
+++ src/routes.js
@@ -8,8 +8,12 @@ export const routes = [
         method: 'GET',
         path: buildRoutePath('/tasks'),
         handler: (req, res) => {
+            const { search } = req.query
 
-            const tasks = database.select('tasks')
+            const tasks = database.select('tasks', search ? {
+                title: search,
+                description: search
+            } : null)
 
             return res.end(JSON.stringify(tasks))
         }
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from 'node:http'
 import { routes } from "./routes.js";
+import {extractQueryParams} from "./utils/extract-query-params.js";
 
 const server = http.createServer(async (req, res) => {
     const { url, method } = req
@@ -10,6 +11,14 @@ const server = http.createServer(async (req, res) => {
     })
 
     if (route) {
+        const routeParams = req.url.match(route.path)
+
+        const { query, ...params } = routeParams.groups
+
+        req.params = params
+
+        req.query = query ? extractQueryParams(query) : {}
+
         return route.handler(req, res)
     }
 
